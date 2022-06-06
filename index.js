e13b5b16c64d15dee93c3ca5dec1f529f9f432bd
ghufran adel <gho0ofran.adel14@gmail.com>
2022-06-06 04:47:45 +0300

    compeleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+// Add your code here
+function submitData(name,email){
+    const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+    };
+    
+    const object ={
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    },
+    body: JSON.stringify(formData)};
+    return fetch(`http://localhost:3000/users`,object)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        document.body.innerHTML=data.id;
+    }).catch(error=> {
+      document.body.innerHTML=error.message;
+    });
+};
+
+submitData()
